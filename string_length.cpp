98980d7f023b046125a3fb62a7638fb8e67f7a3d
Satyajeet Gaur <61581886+the-satyajeet@users.noreply.github.com>
2023-07-12 00:17:27 +0530

    Create string_length.cpp
    This code is to find the length of the string

---
+++ string_length.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()  {
+    int i = 0;
+    // char S[] = "Satyajeet"; 
+    string S = "Gaur";
+
+    for(i = 0; S[i] != '\0';i++)    {
+
+    }
+    cout<<"Length of the string "<<S<<" is "<<i<<endl;
+
+    return 0;
+}
