59d5a69d922aedfe11b5ac72c8d78539a97986bc
Satyajeet Gaur <61581886+the-satyajeet@users.noreply.github.com>
2023-07-19 17:19:35 +0530

    Create array_append_insert_delete.cpp
    This code performs various array operations, such as append, insert, and delete, on a given array. The specific details of the array operations are not provided in the description, but here's a general explanation of each operation:
    
    Append: This operation adds an element to the end of the array, increasing its size by one. The new element is placed at the last index of the array.
    
    Insert: This operation inserts an element at a specified index in the array, shifting existing elements to the right to accommodate the new element.
    
    Delete: This operation removes an element from the array, shifting all elements to the left to close the gap left by the deleted element. The size of the array decreases by one after the deletion.

---
+++ array_append_insert_delete.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+struct Array {
+    int A[10];
+    int size;
+    int length;
+};
+
+// append
+void Append(struct Array *arr, int val) {
+    if (arr->length < arr->size)
+        arr->A[arr->length++] = val;
+}
+
+// insert
+void Insert(struct Array *arr, int val, int index)  {
+    if(index < arr-> length && index >= 0)
+        for (int i = arr->length; i > index; i--)
+            arr->A[i] = arr->A[i-1];
+        arr->A[index] = val;
+        arr->length++;
+}
+
+// delete
+int Delete(struct Array *arr, int index)   {
+    int x = arr->A[index];
+
+    if (index >= 0 && index < arr->length)   {
+        for (int i = index; i < arr->length - 1; i++)
+            arr->A[i] = arr->A[i+1];
+        arr->length--;
+        return x;
+    }
+
+    return 0;
+}
+
+// display
+void Display(struct Array arr)  {
+    for (int i = 0; i < arr.length; i++)
+        cout<<arr.A[i]<<" ";
+
+    cout<<endl;
+}
+
+int main()
+{
+    struct Array arr = {{0, 2, 4, 6, 8}, 10, 5};
+
+    // Append(&arr, 10);
+    // Insert(&arr, 20, 2);
+
+    Display(arr);
+
+    cout<<Delete(&arr, 3)<<endl;
+    Display(arr);
+
+    return 0;
+}
