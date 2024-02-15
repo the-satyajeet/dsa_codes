55c239bf609d03e91c0ac113252f93fc5dd4af13
Satyajeet Gaur <the.satyajeetgaur@gmail.com>
2024-02-15 22:07:50 +0530

    binary search

---
+++ binaryRecursiveSearch.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int binaryRecursiveSearch(int arr[], int n, int low, int high, int target) {
+
+    if (high >= low) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        if (arr[mid] > target)
+            return binaryRecursiveSearch(arr, n, low, mid - 1, target);
+
+        return binaryRecursiveSearch(arr, n, mid + 1, high, target);
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {12, 23, 38, 49, 53, 61, 72, 92};
+    int target;
+
+    cout<<"Enter number to search in array: ";
+    cin>>target;
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int low = 0, high = n-1;
+    int res = binaryRecursiveSearch(arr, n, low, high, target);
+    if(res == -1)
+        cout<<"Not found";
+    else
+        cout<<"Found at "<<res;
+
+    return 0;
+}
