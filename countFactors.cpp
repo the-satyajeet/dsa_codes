32e12e85e8ef678ad095fc363be60789ec4fe25e
Satyajeet Gaur <the.satyajeetgaur@gmail.com>
2024-02-25 00:28:04 +0530

    countFactors

---
+++ countFactors.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countFactors(int num) {
+    int count = 0;
+
+    for (int i = 1; i <= sqrt(num); ++i) {
+        if (num % i == 0) {
+            if (i == (num / i)) {
+                count++;
+            }
+            else {
+                count += 2;
+            }
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n = 36;
+    // int count = 2;
+
+    // for (int i = 2; i < n/2+1; i++) {
+    //     if(n % i == 0)
+    //         count++;
+    // } // not recommended
+    // cout<<count;
+
+    cout<<countFactors(n);
+
+    return 0;
+}
+
