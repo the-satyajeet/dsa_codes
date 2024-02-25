dd34a14f35afad15465f81bb80f91cfc5f43a4ed
Satyajeet Gaur <the.satyajeetgaur@gmail.com>
2024-02-25 12:53:03 +0530

    isPalindrome

---
+++ isPalindrome.cpp
@@ -0,0 +1,26 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(int num) {
+    int og = num;
+    int ans = 0;
+    while(num > 0) {
+        int digit = num % 10;
+        ans = ans * 10 + digit;
+        num /= 10;
+    }
+    return ans == og;
+}
+
+int main() {
+
+    int n = 121;
+    if(isPalindrome(n))
+        cout<<"Palindrom";
+    else
+        cout<<"Not Palindrom";
+
+    return 0;
+
+}
