a10628f2c3adc84315b218eac1affd3b087ad94a
Satyajeet Gaur <the.satyajeetgaur@gmail.com>
2024-02-27 23:48:34 +0530

    Reverse a Linked List

---
+++ reverseList.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* reverseList(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* curr = head;
+    while (curr != nullptr) {
+        ListNode* nextTemp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
+}
+
+int main() {
+    // Example usage
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+
+    head = reverseList(head);
+
+    // Printing reversed list
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+
+    return 0;
+}
