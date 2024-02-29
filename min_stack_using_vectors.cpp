4a2b44d5e0122a0e7942b3a8efcb296afee6c5fe
Satyajeet Gaur <the.satyajeetgaur@gmail.com>
2024-02-29 23:59:37 +0530

    Min Stack using Vectors

---
+++ min_stack_using_vectors.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+class MinStack {
+private:
+    vector<int> data;
+    vector<int> mins;
+public:
+    void push(int x) {
+        data.push_back(x);
+        if (mins.empty() || x <= mins.back()) {
+            mins.push_back(x);
+        }
+    }
+    void pop() {
+        if (data.empty()) {
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        if (data.back() == mins.back()) {
+            mins.pop_back();
+        }
+        data.pop_back();
+    }
+    int top() {
+        if (data.empty()) {
+            cout << "Stack is empty" << endl;
+            return -1; // or throw an exception
+        }
+        return data.back();
+    }
+    int getMin() {
+        if (mins.empty()) {
+            cout << "Stack is empty" << endl;
+            return -1; // or throw an exception
+        }
+        return mins.back();
+    }
+};
+
+int main() {
+    MinStack stack;
+    stack.push(3);
+    stack.push(5);
+    stack.push(2);
+    stack.push(1);
+
+    cout << "Minimum element: " << stack.getMin() << endl;
+    stack.pop();
+    cout << "Minimum element after popping: " << stack.getMin() << endl;
+
+    return 0;
+}
